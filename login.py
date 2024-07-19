a17817e7b2ddf6f74ab3b4bcb9ae8783ac1f1cdc
Romanos Grapsas <romilgrap@gmail.com>
2024-07-19 22:09:19 +0300

    encrypt/decrypt stored password

--- login.py
+++ login.py
@@ -6,11 +6,13 @@ import toml
 from micloud import MiCloud
 from micloud.micloudexception import MiCloudAccessDenied
 
+from password_cipher import PasswordCipher
+
 # define the path to the configuration file
 CONFIG_FILE = "config.toml"
 
 
-def _load_credentials():
+def _load_credentials(password_cipher):
     # function to load credentials from configuration TOML file
 
     username = None
@@ -22,10 +24,20 @@ def _load_credentials():
             username = config.get("credentials", {}).get("username")
             password = config.get("credentials", {}).get("password")
 
+            # decrypt password if it is not None
+            if password:
+                try:
+                    password = password_cipher.decrypt_password(password)
+                except Exception:
+                    # if decryption fails, encrypt the password
+                    password = password_cipher.encrypt_password(password)
+                    _save_credentials(username, password, password_cipher)
+                    password = password_cipher.decrypt_password(password)
+
     return username, password
 
 
-def _save_credentials(username, password):
+def _save_credentials(username, password, password_cipher):
     # function to save credentials to configuration TOML file
 
     config = {}
@@ -34,9 +46,12 @@ def _save_credentials(username, password):
         with open(CONFIG_FILE, "r") as file:
             config = toml.load(file)
 
+    # encrypt the password before saving
+    encrypted_password = password_cipher.encrypt_password(password)
+
     # update credentials in the config dictionary
     config.setdefault("credentials", {})["username"] = username
-    config.setdefault("credentials", {})["password"] = password
+    config.setdefault("credentials", {})["password"] = encrypted_password
 
     # write updated config back to the file
     with open(CONFIG_FILE, "w") as file:
@@ -45,8 +60,10 @@ def _save_credentials(username, password):
 
 def micloud_login():
     saved_credentials = True
+    password_cipher = PasswordCipher()
+
     # load credentials
-    username, password = _load_credentials()
+    username, password = _load_credentials(password_cipher)
 
     # check if credentials are empty and prompt user if necessary
     if not username or not password:
@@ -70,6 +87,6 @@ def micloud_login():
 
     # write credentials to the TOML file only if login was successful
     if login_success and not saved_credentials:
-        _save_credentials(username, password)
+        _save_credentials(username, password, password_cipher)
 
     return mc if login_success else None
